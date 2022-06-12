be1b4508d642f29c3ac5f1d7bc8c7724aa77f188
lizonkisel <ees.hwork@gmail.com>
2022-06-12 18:21:14 +0300

    Updates

---
+++ precache-manifest.fcb9fe6de690f514a344e55a6aecd22d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "62e661bb7db379bed4b5",
+    "url": "/static/css/main.d868b746.chunk.css"
+  },
+  {
+    "revision": "62e661bb7db379bed4b5",
+    "url": "/static/js/main.62e661bb.chunk.js"
+  },
+  {
+    "revision": "e8100a7e04f515f37629",
+    "url": "/static/js/1.e8100a7e.chunk.js"
+  },
+  {
+    "revision": "605180e446c859607955",
+    "url": "/static/css/2.29c5ec67.chunk.css"
+  },
+  {
+    "revision": "605180e446c859607955",
+    "url": "/static/js/2.605180e4.chunk.js"
+  },
+  {
+    "revision": "69b11a1d0ee090c85547",
+    "url": "/static/js/runtime~main.69b11a1d.js"
+  },
+  {
+    "revision": "04891082df8333e3b0e2042f65e83ca8",
+    "url": "/static/media/done.04891082.png"
+  },
+  {
+    "revision": "21ba2c11275a07ccf3890fbeab585525",
+    "url": "/static/media/JetBrainsMono-Regular.21ba2c11.woff2"
+  },
+  {
+    "revision": "44e2d48cffc1c5da01730f6103fe9eaa",
+    "url": "/static/media/iceland.44e2d48c.woff2"
+  },
+  {
+    "revision": "2fe9a2bdaa6daf61e770a2edb7252040",
+    "url": "/static/media/JetBrainsMono-Regular.2fe9a2bd.woff"
+  },
+  {
+    "revision": "896f23850b33cd1df94eb955d4b60079",
+    "url": "/static/media/JetBrainsMono-Bold.896f2385.woff"
+  },
+  {
+    "revision": "df042be2e40babc665f1f8a2d67d0f86",
+    "url": "/static/media/iceland.df042be2.woff"
+  },
+  {
+    "revision": "a492f7427b1561cc96ace88f5756d94e",
+    "url": "/static/media/JetBrainsMono-Bold.a492f742.woff2"
+  },
+  {
+    "revision": "3ac33dc6ff29dd5971c87749b1d8d325",
+    "url": "/index.html"
+  }
+];
