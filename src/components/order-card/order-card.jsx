3c6d3a82f9e3136d4379b25ded7538b0dd5ff222
lizonkisel <ees.hwork@gmail.com>
2022-07-19 15:29:38 +0300

    feat: configure moment.js library for date in orders feed cards

--- src/components/order-card/order-card.jsx
+++ src/components/order-card/order-card.jsx
@@ -1,28 +1,30 @@
 import React from "react";
 import IngredientMini from "../ingredient-mini/ingredient-mini.jsx";
 import moment from 'moment';
+import 'moment/locale/ru';
 
 import styles from './order-card.module.css';
 
 export default function OrderCard({ order }) {
+  moment.locale('ru', {
+    calendar : {
+      lastDay : '[Вчера,] LT',
+      sameDay : '[Сегодня,] LT',
+      // lastWeek: '[Прошлый] dddd',
+      sameElse: 'DD/MM/YYYY',
+  }
+  });
 
-  moment().format();
-  console.log(moment);
-  console.log(order);
   const number = order.number;
-  // const date = order.createdAt;
-  const date = new Date(Date.parse(order.createdAt)).toLocaleString();
+  const date = Date.parse(order.createdAt);
   const name = order.name;
   const ingredients = order.ingredients;
-  console.log(ingredients);
-
-  console.log(date);
 
   return (
     <article className={styles.card}>
       <div className={styles.order_data}>
         <span className="text text_type_digits-default">#{number}</span>
-        <time>{date}</time>
+        <time className="text text_type_main-default text_color_inactive">{`${moment(date).utcOffset("+03:00").calendar()} i-GMT+3`}</time>
       </div>
       <h2 className={`text text_type_main-medium ${styles.order_header}`}>{name}</h2>
       <div className={styles.ingredients}>
